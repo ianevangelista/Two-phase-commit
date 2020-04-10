4c5f1abf8eb5021e63fa5715660707690a95737b
NikolaiDokken <nikolaidokken@live.no>
2020-04-10 17:05:26 +0200

    Made server run forever, and added more feedback

--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,7 @@
 
 import java.io.*;
 import java.net.*;
+
 public class Client implements Runnable
 {
     static Socket clientSocket = null;
@@ -9,8 +10,7 @@ public class Client implements Runnable
     static DataInputStream is = null;
     static BufferedReader inputLine = null;
     static boolean closed = false;
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int port_number=1111;
         String host="localhost";
         try {
@@ -18,45 +18,37 @@ public class Client implements Runnable
             inputLine = new BufferedReader(new InputStreamReader(System.in));
             os = new PrintStream(clientSocket.getOutputStream());
             is = new DataInputStream(clientSocket.getInputStream());
-        } catch (Exception e)
-        {   System.out.println("Exception occurred : "+e.getMessage()); }
+        } catch (Exception e) {
+            System.out.println("Exception occurred : " + e.getMessage());
+        }
 
-
-        if (clientSocket != null && os != null && is != null)
-        {
-            try
-            {
+        if (clientSocket != null && os != null && is != null) {
+            try {
                 new Thread(new Client()).start();
-                while (!closed)
-                {
+                while (!closed) {
                     os.println(inputLine.readLine());
                 }
                 os.close();
                 is.close();
                 clientSocket.close();
-            } catch (IOException e)
-            {
+            } catch (IOException e) {
                 System.err.println("IOException:  " + e);
             }
         }
     }
     @SuppressWarnings("deprecation")
-    public void run()     {
+    public void run() {
         String responseLine;
-        try
-        {
-            while ((responseLine = is.readLine()) != null)
-            {
+        try {
+            while ((responseLine = is.readLine()) != null) {
                 System.out.println("\n"+responseLine);
-                if (responseLine.equalsIgnoreCase("GLOBAL_COMMIT")==true || responseLine.equalsIgnoreCase("GLOBAL_ABORT")==true )
-                {
+                if (responseLine.equalsIgnoreCase("GLOBAL_COMMIT")==true || responseLine.equalsIgnoreCase("GLOBAL_ABORT")==true ) {
                     break;
                 }
             }
             closed=true;
         }
-        catch (IOException e)
-        {
+        catch (IOException e) {
             System.err.println("IOException:  " + e);
         }
     }
--- src/Server.java
+++ src/Server.java
@@ -7,6 +7,8 @@ public class Server {
     boolean closed = false, inputFromAll = false;
     List<ClientThread> thread;
     List<String> data;
+    String serverName = "Per";
+    String oldServerName = "";
 
     Server() {
         thread = new ArrayList<ClientThread>();
@@ -22,6 +24,7 @@ public class Server {
         try
         {
             serverSocket = new ServerSocket(port_number);
+            System.out.println("Navnet mitt er " + server.serverName);
         } catch (IOException e) {
             System.out.println(e);
         }
@@ -32,6 +35,7 @@ public class Server {
                 ClientThread clientThread = new ClientThread(server, clientSocket);
                 (server.thread).add(clientThread);
                 System.out.println("\nNow Total clients are : " + (server.thread).size());
+
                 (server.data).add("NOT_SENT");
                 clientThread.start();
             } catch (IOException e) { }
@@ -70,6 +74,16 @@ class ClientThread extends Thread
             name = is.readLine();
             clientIdentity = name;
             os.println("Welcome " + name + " to this 2 Phase Application.\nYou will receive a vote Request now...");
+            // Simulate decision
+            if (server.thread.size() == 1) {
+                server.oldServerName = server.serverName;
+                if (server.serverName.equals("Ola")) {
+                    server.serverName = "Per"; // dette skjedde egt før
+                } else {
+                    server.serverName = "Ola"; // dette skjedde egt før
+                }
+            }
+            os.println("Serverens navn er endret til " + server.serverName + ". Beholde endringen?");
             os.println("VOTE_REQUEST\nPlease enter COMMIT or ABORT to proceed : ");
             for (int i = 0; i < (server.thread).size(); i++)
             {
@@ -85,12 +99,16 @@ class ClientThread extends Thread
                 {
                     System.out.println("\nFrom '" + clientIdentity
                             + "' : ABORT\n\nSince aborted we will not wait for inputs from other clients.");
-                    System.out.println("\nAborted....");
+                    server.serverName = server.oldServerName;
+                    System.out.println("\nAborted....Navnet mitt er fortsatt " + server.serverName);
 
                     for (int i = 0; i < (server.thread).size(); i++) {
                         ((server.thread).get(i)).os.println("GLOBAL_ABORT");
                         ((server.thread).get(i)).os.close();
                         ((server.thread).get(i)).is.close();
+                        server.data.remove(server.thread.indexOf(server.thread.get(i)));
+                        server.thread.remove(i);
+
                     }
                     break;
                 }
@@ -115,19 +133,23 @@ class ClientThread extends Thread
                         }
                         if (server.inputFromAll)
                         {
-                            System.out.println("\n\nCommited....");
+                            server.oldServerName = server.serverName;
+                            System.out.println("\n\nCommited.... Navnet mitt er nå " + server.serverName);
                             for (int i = 0; i < (server.thread).size(); i++)
                             {
                                 ((server.thread).get(i)).os.println("GLOBAL_COMMIT");
                                 ((server.thread).get(i)).os.close();
                                 ((server.thread).get(i)).is.close();
+                                server.data.remove(server.thread.indexOf(server.thread.get(i)));
+                                server.thread.remove(i);
+
                             }
                             break;
                         }
                     } // if thread.contains
                 } // commit
             } // while
-            server.closed = true;
+            // server.closed = true;
             clientSocket.close();
         } catch (IOException e) { }
 
