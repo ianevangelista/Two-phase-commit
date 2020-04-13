f83dcc7e05697869adcc19f269a30c068f855f4b
ianevangelista <ianevangelista1999@gmail.com>
2020-04-13 14:35:56 +0200

    translated variable names and output

--- src/Client.java
+++ src/Client.java
@@ -5,32 +5,32 @@ import java.net.*;
 
 public class Client implements Runnable
 {
-    static Socket clientSocket = null;
+    static Socket klientSocket = null;
     static PrintStream os = null;
     static DataInputStream is = null;
-    static BufferedReader inputLine = null;
-    static boolean closed = false;
+    static BufferedReader inputLinje = null;
+    static boolean lukket = false;
     public static void main(String[] args) {
-        int port_number=1111;
+        int port=1111;
         String host="localhost";
         try {
-            clientSocket = new Socket(host, port_number);
-            inputLine = new BufferedReader(new InputStreamReader(System.in));
-            os = new PrintStream(clientSocket.getOutputStream());
-            is = new DataInputStream(clientSocket.getInputStream());
+            klientSocket = new Socket(host, port);
+            inputLinje = new BufferedReader(new InputStreamReader(System.in));
+            os = new PrintStream(klientSocket.getOutputStream());
+            is = new DataInputStream(klientSocket.getInputStream());
         } catch (Exception e) {
             System.out.println("Exception occurred : " + e.getMessage());
         }
 
-        if (clientSocket != null && os != null && is != null) {
+        if (klientSocket != null && os != null && is != null) {
             try {
                 new Thread(new Client()).start();
-                while (!closed) {
-                    os.println(inputLine.readLine());
+                while (!lukket) {
+                    os.println(inputLinje.readLine());
                 }
                 os.close();
                 is.close();
-                clientSocket.close();
+                klientSocket.close();
             } catch (IOException e) {
                 System.err.println("IOException:  " + e);
             }
@@ -38,15 +38,15 @@ public class Client implements Runnable
     }
     @SuppressWarnings("deprecation")
     public void run() {
-        String responseLine;
+        String responseLinje;
         try {
-            while ((responseLine = is.readLine()) != null) {
-                System.out.println("\n"+responseLine);
-                if (responseLine.equalsIgnoreCase("GLOBAL_COMMIT")==true || responseLine.equalsIgnoreCase("GLOBAL_ABORT")==true ) {
+            while ((responseLinje = is.readLine()) != null) {
+                System.out.println("\n"+responseLinje);
+                if (responseLinje.equalsIgnoreCase("GLOBAL_COMMIT")==true || responseLinje.equalsIgnoreCase("GLOBAL_ABORT")==true ) {
                     break;
                 }
             }
-            closed=true;
+            lukket=true;
         }
         catch (IOException e) {
             System.err.println("IOException:  " + e);
---
+++ src/Tjener.java
@@ -0,0 +1,156 @@
+//Two Phase Commit Protocol SERVER
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Tjener {
+    boolean lukket = false, inputFraAlle = false;
+    List<KlientTraad> traadListe;
+    List<String> data;
+    String tjenerNavn = "Per";
+    String tjenerNavnGammel = "";
+
+    Tjener() {
+        traadListe = new ArrayList<KlientTraad>();
+        data = new ArrayList<String>();
+    }
+
+    public static void main(String args[]) {
+        Socket klientSocket = null;
+        ServerSocket tjenerSocket = null;
+        int port_number = 1111;
+        Tjener tjener = new Tjener();
+        try {
+            tjenerSocket = new ServerSocket(port_number);
+            System.out.println("Navnet mitt er " + tjener.tjenerNavn);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        while (!tjener.lukket) {
+            try {
+                klientSocket = tjenerSocket.accept();
+                KlientTraad klientTraad = new KlientTraad(tjener, klientSocket);
+                (tjener.traadListe).add(klientTraad);
+                System.out.println("\nAntall klienter er oppdatert til: " + (tjener.traadListe).size());
+
+                (tjener.data).add("NOT_SENT");
+                klientTraad.start();
+            } catch (IOException e) { }
+        }
+        try {
+
+            tjenerSocket.close();
+        } catch (Exception e1) { }
+    } // end main
+} // end class Server
+
+class KlientTraad extends Thread {
+    DataInputStream is = null;
+    String linje;
+    String navn;
+    PrintStream os = null;
+    Socket klientSocket = null;
+    String klientIdentitet;
+    Tjener tjener;
+
+    public KlientTraad(Tjener tjener, Socket klientSocket) {
+        this.klientSocket = klientSocket;
+        this.tjener = tjener;
+    }
+
+    @SuppressWarnings("deprecation")
+    public void run() {
+        try {
+            is = new DataInputStream(klientSocket.getInputStream());
+            os = new PrintStream(klientSocket.getOutputStream());
+            os.println("Skriv inn navnet ditt: ");
+            navn = is.readLine();
+            klientIdentitet = navn;
+            os.println("Velkommen " + navn + " til denne 2-fase applikasjonen.\nDu vil motta en VOTE_REQUEST...");
+            // Simulerer avgjørelse
+            if (tjener.traadListe.size() == 1) {
+                tjener.tjenerNavnGammel = tjener.tjenerNavn;
+                if (tjener.tjenerNavn.equals("Ola")) {
+                    tjener.tjenerNavn = "Per"; // dette skjedde egt før
+                } else {
+                    tjener.tjenerNavn = "Ola"; // dette skjedde egt før
+                }
+            }
+            os.println("Tjenerens navn er endret til " + tjener.tjenerNavn + ". Beholde endringen?");
+            os.println("VOTE_REQUEST\nVennligst skriv inn COMMIT eller ABORT: ");
+            for (int i = 0; i < (tjener.traadListe).size(); i++) {
+                if ((tjener.traadListe).get(i) != this) {
+                    ((tjener.traadListe).get(i)).os.println("---En ny bruker ved navn " + navn + " har blitt med i applikasjonen---");
+                }
+            }
+            while (true) {
+                linje = is.readLine();
+                if (linje.equalsIgnoreCase("ABORT")) {
+                    System.out.println("\nFra '" + klientIdentitet
+                            + "' : ABORT\n\nSiden det ble skrevet ABORT, vil vi ikke vente paa flere input fra andre klienter.");
+                    tjener.tjenerNavn = tjener.tjenerNavnGammel;
+                    System.out.println("\nAborted....Navnet mitt er fortsatt " + tjener.tjenerNavn);
+
+                    for (int i = 0; i < (tjener.traadListe).size(); i++) {
+                        ((tjener.traadListe).get(i)).os.println("GLOBAL_ABORT");
+                        ((tjener.traadListe).get(i)).os.close();
+                        ((tjener.traadListe).get(i)).is.close();
+                        tjener.data.remove(tjener.traadListe.indexOf(tjener.traadListe.get(i)));
+                        tjener.traadListe.remove(i);
+
+                    }
+                    break;
+                }
+                if (linje.equalsIgnoreCase("COMMIT")) {
+                    System.out.println("\nFra '" + klientIdentitet + "' : COMMIT");
+                    if ((tjener.traadListe).contains(this)) {
+                        (tjener.data).set((tjener.traadListe).indexOf(this), "COMMIT");
+                        for (int j = 0; j < (tjener.data).size(); j++) {
+                            if (!(((tjener.data).get(j)).equalsIgnoreCase("NOT_SENT"))) {
+                                tjener.inputFraAlle = true;
+                                continue;
+                            } else {
+                                tjener.inputFraAlle = false;
+                                System.out.println("\nVenter paa input fra andre klienter.");
+                                break;
+                            }
+                        }
+                        if (tjener.inputFraAlle) {
+                            tjener.tjenerNavnGammel = tjener.tjenerNavn;
+                            System.out.println("\n\nCommited.... Navnet mitt er naa " + tjener.tjenerNavn);
+                            for (int i = 0; i < (tjener.traadListe).size(); i++) {
+                                ((tjener.traadListe).get(i)).os.println("GLOBAL_COMMIT");
+                                ((tjener.traadListe).get(i)).os.close();
+                                ((tjener.traadListe).get(i)).is.close();
+                                tjener.data.remove(tjener.traadListe.indexOf(tjener.traadListe.get(i)));
+                                tjener.traadListe.remove(i);
+                            }
+                            break;
+                        }
+                    } // if traadListe.contains
+                } // commit
+            } // while
+            // tjener.lukket = true;
+            klientSocket.close();
+        } catch (IOException e) { }
+    }
+}// end class KlientTraad
+
+/*
+Coordinator                                          Cohorts
+                            QUERY TO COMMIT
+                -------------------------------->
+                              VOTE YES/NO           prepare/abort
+                <-------------------------------
+commit/abort                 COMMIT/ROLLBACK
+                -------------------------------->
+                              ACKNOWLEDGMENT        commit/abort
+                <--------------------------------
+end
+
+ Two Phases :
+ 1.Prepare and Vote Phase
+ 2. Commit or Abort Phase
+
+ "Either All Commit Or All RollBack."
+ */
